3a63df4d1cdc919874e06b048bbf994b0000ed36
duartecgustavo@outlook.com <duartecgustavo@outlook.com>
2020-12-01 16:52:27 -0300

    att

---
+++ fse-react/src/components/App/index.jsx
@@ -0,0 +1,28 @@
+
+// Dependencias e Bibliotecas
+import React from 'react';
+
+// Componentes
+import Header from '../Header';
+import Footer from '../Footer';
+
+// CSS
+import "./styles.css"
+
+
+const App = () => {
+
+  return (
+
+    <>
+      <Header/>
+        <main>
+          Isso é o conteúdo!
+        </main>
+      <Footer/>
+    </>
+
+  );
+}
+
+export default App;
---
+++ fse-react/src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const Footer = () => {
+
+    return (
+        <footer className="container-fluid font-weight-bold">
+        
+        <div className="text-footer container text-center d-flex align-items-center justify-content-center">
+            ©2020 - Recode Pró - Full-Stack-Eletro
+        </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
