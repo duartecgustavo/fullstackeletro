0b9aaa72daa38d1974455eda05e2e6392811551f
duartecgustavo@outlook.com <duartecgustavo@outlook.com>
2020-12-04 22:32:26 -0300

    att

--- fse-react/src/components/Produtos/selecionarProd.jsx
+++ fse-react/src/components/Produtos/selecionarProd.jsx
@@ -46,22 +46,17 @@ const SelectProducts = () => {
     return (
 
         <>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
             <nav className="menu-categoria desktop container d-flex align-items-center justify-content-center mt-4 shadow">
             <div className="row">
 
 
                 <div className="row">
                 {/* <a className="text-categoria font-weight-bold text-white text-center mx-3" onclick={todasCategorias}>TODOS</a> */}
-                <a className="text-categoria font-weight-bold text-white text-center mx-3" id={1} onclick={filtroProd}>GELADEIRAS</a>
-                <a className="text-categoria font-weight-bold text-white text-center mx-3" id={2} onclick={filtroProd}>FOGÕES</a>
-                <a className="text-categoria font-weight-bold text-white text-center mx-3" id={3} onclick={filtroProd}>LAVA-ROUPA</a>
-                <a className="text-categoria font-weight-bold text-white text-center mx-3" id={4} onclick={filtroProd}>MICROONDAS</a>
-                <a className="text-categoria font-weight-bold text-white text-center mx-3" id={5} onclick={filtroProd}>FREEZER</a>
+                <a className="text-categoria font-weight-bold text-white text-center mx-3" onclick={filtroProd}>GELADEIRAS</a>
+                <a className="text-categoria font-weight-bold text-white text-center mx-3" onclick={filtroProd}>FOGÕES</a>
+                <a className="text-categoria font-weight-bold text-white text-center mx-3" onclick={filtroProd}>LAVA-ROUPA</a>
+                <a className="text-categoria font-weight-bold text-white text-center mx-3" onclick={filtroProd}>MICROONDAS</a>
+                <a className="text-categoria font-weight-bold text-white text-center mx-3" onclick={filtroProd}>FREEZER</a>
                 <a className="text-categoria font-weight-bold text-white text-center mx-3">EXAUSTORES</a>
                 </div>
 
