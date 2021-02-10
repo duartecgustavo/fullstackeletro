3fb19eee08ad421b2ee633eb2f3a1b66fc824c14
Gustavo Castanho <65131471+duartecgustavo@users.noreply.github.com>
2021-02-10 19:33:10 -0300

    att

--- fse-react/backend/models/comentarios.js
+++ fse-react/backend/models/comentarios.js
@@ -1,28 +1,45 @@
-const Conn = require ('../config/db');
+const { Schema, model } = require('mongoose')
 
-const Comentario = function(comentario){
-    this.nome = comentario.nome;
-    this.msg = comentario.msg;
-}
+const CommentSchema = new Schema({
+    name: {
+        type: String
+    },
+    msg: {
+        type: String
+    }
+})
 
-Comentario.getAll = result => {
-    const sql =  "SELECT * FROM comentarios";
-    Conn.query(sql, (error, res) => {
-        if (error){
-            return error
-        } console.log("Comentario: ", res);
-        result(null,res);
-    })
-}
+schemaModel = model('comentarios', CommentSchema)
 
-Comentario.create = (newComentario, result) => {
-    const sql = `INSERT INTO comentarios SET ?`;
-    Conn.query(sql, newComentario, (error, res) => {
-        if(error){
-            result(true, {error: "Mensagem não enviada!"});
-        } console.log('comentario enviado: '); 
-        result(null,{id: res.insertId, ...newComentario})
-    })
-}
+module.exports = schemaModel;
 
-module.exports = Comentario;
+
+
+// const Conn = require ('../config/db');
+
+// const Comentario = function(comentario){
+//     this.nome = comentario.nome;
+//     this.msg = comentario.msg;
+// }
+
+// Comentario.getAll = result => {
+//     const sql =  "SELECT * FROM comentarios";
+//     Conn.query(sql, (error, res) => {
+//         if (error){
+//             return error
+//         } console.log("Comentario: ", res);
+//         result(null,res);
+//     })
+// }
+
+// Comentario.create = (newComentario, result) => {
+//     const sql = `INSERT INTO comentarios SET ?`;
+//     Conn.query(sql, newComentario, (error, res) => {
+//         if(error){
+//             result(true, {error: "Mensagem não enviada!"});
+//         } console.log('comentario enviado: '); 
+//         result(null,{id: res.insertId, ...newComentario})
+//     })
+// }
+
+// module.exports = Comentario;
--- fse-react/backend/routes/comentarios.routes.js
+++ fse-react/backend/routes/comentarios.routes.js
@@ -1,6 +1,22 @@
 module.exports = server => {
-    const comentario = require('../controllers/comentarios');
-    server.get('/comentarios', comentario.findAll);
-    server.post('/comentarios', comentario.create);
+    Comments = require('../models/comentarios');
+
+    server.post('/comentarios', async (req, res) => {
+        const { name, msg } = req.body;
+        const result = await Comments.create({ name, msg });
+        res.send(result)
+    })
+
+    server.get('/comentarios', async (req, res) => {
+        const result = await Comments.find();
+        res.send(result)
+    })
 }
+
+
+// module.exports = server => {
+//     const comentario = require('../controllers/comentarios');
+//     server.get('/comentarios', comentario.findAll);
+//     server.post('/comentarios', comentario.create);
+// }
 
